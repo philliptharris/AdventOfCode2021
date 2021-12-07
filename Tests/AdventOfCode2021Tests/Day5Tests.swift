4b5f168e4d5d3e3ce876285418ec4dd8de5175ea
Phillip Harris <philliptharris@gmail.com>
2021-12-06 19:45:23 -0500

    Add Day 5 puzzle solutions

--- Tests/AdventOfCode2021Tests/Day5Tests.swift
+++ Tests/AdventOfCode2021Tests/Day5Tests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class Day5Tests: XCTestCase {
     
-    struct Point {
+    struct Point: Hashable {
         let x: Int
         let y: Int
         init(_ string: String) {
@@ -11,6 +11,10 @@ final class Day5Tests: XCTestCase {
             guard coordinates.count == 2 else { fatalError() }
             x = coordinates.first!
             y = coordinates.last!
+        }
+        init(x: Int, y: Int) {
+            self.x = x
+            self.y = y
         }
     }
     
@@ -28,6 +32,24 @@ final class Day5Tests: XCTestCase {
         }
         func isVertical() -> Bool {
             a.x == b.x
+        }
+        func pointsFromAtoB() -> [Point] {
+            let xVals = Array(stride(from: a.x, through: b.x, by: b.x > a.x ? 1 : -1))
+            let yVals = Array(stride(from: a.y, through: b.y, by: b.y > a.y ? 1 : -1))
+            var points: [Point] = []
+            if a.x == b.x {
+                points = yVals.map { Point(x: a.x, y: $0) }
+            } else if a.y == b.y {
+                points = xVals.map { Point(x: $0, y: a.y) }
+            } else {
+                guard xVals.count == yVals.count else { fatalError() }
+                for i in 0..<xVals.count {
+                    let x = xVals[i]
+                    let y = yVals[i]
+                    points.append(Point(x: x, y: y))
+                }
+            }
+            return points
         }
     }
     
@@ -58,5 +80,28 @@ final class Day5Tests: XCTestCase {
         let pointsWhereAtLeastTwoLinesOverlap = field.joined().filter { $0 >= 2 }
         
         XCTAssertEqual(pointsWhereAtLeastTwoLinesOverlap.count, 7438)
+    }
+    
+    func testDiagonalLines() throws {
+        
+        let lines = try Utility.readLines(from: "Day5.txt").map { Line($0) }
+        
+        var field: [Point: Int] = [:]
+        (0..<1000).forEach { row in
+            (0..<1000).forEach { col in
+                let point = Point(x: col, y: row)
+                field[point] = 0
+            }
+        }
+        
+        lines.forEach { line in
+            line.pointsFromAtoB().forEach { point in
+                field[point]! += 1
+            }
+        }
+        
+        let pointsWhereAtLeastTwoLinesOverlap = field.filter { $0.value >= 2 }
+        
+        XCTAssertEqual(pointsWhereAtLeastTwoLinesOverlap.count, 21406)
     }
 }
